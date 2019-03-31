debf855e53c637c01bb65c3693888232b8512ca3
denshade <lieven.vaneeckhaute@gmail.com>
2019-03-31 13:15:45 +0200

    use python and du together => .145 second iso 10 sec

--- dupc.sh
+++ dupc.sh
@@ -4,13 +4,4 @@ if [ $# -ne 1 ]; then
  exit 1
 fi
 cd $1
-totalSize=$(du -s . | cut -f 1)
-echo "total size $totalSize"
-dulines=$(du . | sort -n)
-while read -r duline; 
-do 
-   size=$(echo $duline | cut -d" " -f 1)
-   percent=$((size * 100 / totalSize))
-   label=$(echo $duline | cut -d" " -f 2-)
-   echo "$percent $label"
-done <<< "$dulines"
+du . | sort -n | python dupc.py
